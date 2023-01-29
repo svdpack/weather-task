f4da70fb3d790d4281157a9ff580d35dc0eeff1d
ymnik <v.s@dcjudgement.com>
2023-01-30 00:57:54 +0100

    add composer

--- bootstrap.php
+++ bootstrap.php
@@ -6,19 +6,5 @@ if (!defined('PATH_CORE')) {
 
 require_once PATH_ROOT.'config/mainConfig.php';
 
-require_once PATH_ROOT.'app/helpers.php';
-
-spl_autoload_register(function ($name_class_all) {
-    $nameClassArray = explode('\\', $name_class_all);
-
-    if (!empty($nameClassArray[0]) && $nameClassArray[0] === 'App') {
-        $nameClassArray[0] = 'app';
-
-        $pathClass = realpath(PATH_ROOT.implode(DIRECTORY_SEPARATOR, $nameClassArray).".php");
-        if (file_exists($pathClass)) {
-            require_once $pathClass;
-        }
-    }
-});
-
+require PATH_ROOT.'vendor/autoload.php';
 
